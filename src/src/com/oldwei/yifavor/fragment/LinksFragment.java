3276a0b777dae5f6dec578aa5b3c7d4179420690
David Wei <davidwei@augmentum.com.cn>
2014-06-06 18:20:38 +0800

    Add the drag sort library.

--- src/src/com/oldwei/yifavor/fragment/LinksFragment.java
+++ src/src/com/oldwei/yifavor/fragment/LinksFragment.java
@@ -12,16 +12,21 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Toast;
 
+import com.mobeta.android.dslv.DragSortController;
+import com.mobeta.android.dslv.DragSortListView;
+import com.mobeta.android.dslv.DragSortListView.DropListener;
+import com.mobeta.android.dslv.DragSortListView.RemoveListener;
 import com.oldwei.yifavor.R;
 import com.oldwei.yifavor.activity.WebActivity;
 import com.oldwei.yifavor.adapter.LinkListAdapter;
 import com.oldwei.yifavor.model.LinkModel;
-import com.oldwei.yifavor.view.SwipeDismissListView;
 
 public class LinksFragment extends Fragment {
 
-    private SwipeDismissListView mListView;
+    private DragSortListView mListView;
+    private DragSortController mController;
     private View mView;
     private BaseAdapter mListAdapter;
     private List<LinkModel> mLinkModelList;
@@ -31,11 +36,12 @@ public class LinksFragment extends Fragment {
         mView = inflater.inflate(R.layout.home_link_fragment, null);
         initView();
         initData();
+        initListeners();
         return mView;
     }
 
     private void initView() {
-        mListView = (SwipeDismissListView) mView.findViewById(R.id.home_link_listview);
+        mListView = (DragSortListView) mView.findViewById(R.id.home_link_listview);
     }
 
     private void initData() {
@@ -43,6 +49,38 @@ public class LinksFragment extends Fragment {
         mListAdapter = new LinkListAdapter(getActivity(), mLinkModelList);
         mListView.setAdapter(mListAdapter);
         mListView.setOnItemClickListener(mListViewItemClickListener);
+        mController = buildController(mListView);
+        mListView.setFloatViewManager(mController);
+        mListView.setOnTouchListener(mController);
+    }
+
+    private void initListeners() {
+        mListView.setDropListener(new DropListener() {
+
+            @Override
+            public void drop(int from, int to) {
+                Toast.makeText(getActivity(), "from: " + from + " ，to: " + to, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mListView.setRemoveListener(new RemoveListener() {
+
+            @Override
+            public void remove(int which) {
+                Toast.makeText(getActivity(), "which " + which, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public DragSortController buildController(DragSortListView dslv) {
+        // defaults are
+        // dragStartMode = onDown
+        // removeMode = flingRight
+        DragSortController controller = new DragSortController(dslv);
+        controller.setRemoveEnabled(true);
+        controller.setSortEnabled(true);
+        controller.setDragInitMode(DragSortController.ON_LONG_PRESS);
+        controller.setRemoveMode(DragSortController.FLING_REMOVE);
+        return controller;
     }
 
     /**
